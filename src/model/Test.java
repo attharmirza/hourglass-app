c6d6431db82a3f8fccbc61f38b8bd0cd8d642347
Atthar Mirza <atthar.mirza@gmail.com>
2025-11-05 02:53:28 -0500

    setting up directory structure

---
+++ src/model/Test.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Test {
+  String name;
+
+  public Test() {
+    this.name = "Untitled";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
